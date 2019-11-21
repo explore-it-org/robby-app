f31516ccc770491f7f454d075a2369e5cb4e4676
Robin Schoch <robin.schoch@fhnw.ch>
2019-11-21 11:37:43 +0100

    fixed #110

--- src/programmingtabs/blockprogramming/ActiveBlockReducer.js
+++ src/programmingtabs/blockprogramming/ActiveBlockReducer.js
@@ -15,6 +15,7 @@ export const ActiveBlockReducer = (state = default_state_block, action) => {
             return Object.assign({}, state, {
                 lastUpdate: Date.now(),
                 selectedBlockIndex: -1,
+                possibleChildren: Database.findAll(),
                 Active_Block: new Program('', ProgramType.BLOCKS, [], [new Block(0, 0)]),
             });
         case ActionTypes.CHANGE_BLOCK_NAME:
