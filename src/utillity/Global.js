f40cdc5d4a0ecd2a796ba93e86643113bb0f163d
Robin Schoch <robin.schoch@fhnw.ch>
2019-11-24 16:34:46 +0100

    1.0.0

--- src/utillity/Global.js
+++ src/utillity/Global.js
@@ -1,7 +1,5 @@
-import packageJson from '../../package';
-
 export default {
     APP_NAME: 'explore-it Robotics',
     SHORT_APP_NAME: 'Robotics',
-    VERSION: packageJson.version,
+    VERSION: '1.0.0',
 };
