7f21956ccaa36483a578ffed1d619388f2ad7f3b
Robin Schoch <robin.schoch@fhnw.ch>
2019-12-17 10:53:34 +0100

    added safe space for ios

--- src/controls/NumericInput.js
+++ src/controls/NumericInput.js
@@ -51,7 +51,7 @@ export default class NumericInput extends Component {
 
 const styles = StyleSheet.create({
     input: {
-        fontFamily: 'Jost-400-Book',
+        fontFamily: 'Jost-Book',
         width: '100%',
         height: '100%',
         borderRadius: 5,
