40d290a737e3ffaea2b0bcd318943e5438ed9472
Jonas Suter <jonas.suter@fhnw.ch>
2020-01-31 10:37:57 +0100

    fixed #235

--- src/settings/SettingsComponent.js
+++ src/settings/SettingsComponent.js
@@ -74,6 +74,8 @@ class SettingsComponent extends Component {
         if (parseInt(duration) > 80) {
             Alert.alert(i18n.t('Settings.error'), i18n.t('Settings.tooBig'));
             newText = '80';
+        }else if(parseInt(duration) === 0){
+            newText = '1';
         } else {
             for (let i = 0; i < duration.length; i++) {
                 if (numbers.indexOf(duration[i]) > -1) {
