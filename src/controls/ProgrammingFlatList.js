18ffd2511c89b772c59d9cfaf4dcde0d4a757670
JonasDev99 <jonas.suter@fhnw.ch>
2020-06-08 17:04:13 +0200

    fixed android build and fixed scrollview / flatlist behaviour on ios and android

--- src/controls/ProgrammingFlatList.js
+++ src/controls/ProgrammingFlatList.js
@@ -1,41 +1,41 @@
-import {Component} from 'react';
-import {StyleSheet, View, Alert, TextComponent, Platform} from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, View, Alert, TextComponent, Platform } from 'react-native';
 import React from 'react';
 import NumericInput from './NumericInput';
 import i18n from '../../resources/locales/i18n';
 import SinglePickerMaterialDialog from '../materialdialog/SinglePickerMaterialDialog';
-import {Text} from 'react-native-paper';
-import {Button, FAB, IconButton} from 'react-native-paper';
+import { Text } from 'react-native-paper';
+import { Button, FAB, IconButton } from 'react-native-paper';
 import CustomIcon from '../utillity/CustomIcon';
 import { TouchableOpacity, ScrollView, FlatList } from 'react-native-gesture-handler';
 
 
 export default class ProgrammingFlatList extends Component {
 
-   renderAndroidFlatList(){
-    return (
-        <ScrollView>
-            {this.renderFlatList()}
-        </ScrollView>
-    )
-   }
+    renderAndroidFlatList() {
+        return (
+            <ScrollView>
+                {this.renderFlatList()}
+            </ScrollView>
+        )
+    }
 
-   renderFlatList(){
-    return (
-        <FlatList
-            data={this.props.data}
-            keyExtractor={this.props.keyExtractor}
-            ref={this.props.ref}
-            renderItem={this.props.renderItem}
-        />
-    )
-   }
+    renderFlatList() {
+        return (
+            <FlatList
+                data={this.props.data}
+                keyExtractor={this.props.keyExtractor}
+                ref={this.props.ref}
+                renderItem={this.props.renderItem}
+            />
+        )
+    }
 
     render() {
-        if(Platform.OS === 'android'){
-            return this.renderAndroidFlatList();
-        }else{
+        if (Platform.OS === 'ios') {
             return this.renderFlatList();
+        } else {
+            return this.renderAndroidFlatList();
         }
     }
 }
