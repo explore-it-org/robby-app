aad5214539787310f52818b4f65d812c1a260f17
Markus Gasser <markus.gasser@jakku.ch>
2025-12-20 12:59:49 +0100

    Fresh start

--- ios/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  Robotics
-//
-//  Created by IMVS1903281 on 28.02.20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
--- src/utillity/Global.js
+++
@@ -1,5 +0,0 @@
-export default {
-    APP_NAME: 'explore-it',
-    SHORT_APP_NAME: 'explore-it',
-    VERSION: '2.3.11',
-};
